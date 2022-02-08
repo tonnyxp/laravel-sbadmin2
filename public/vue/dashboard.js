bc184e7d836a2878bc9f9c2c46a0fe3eedb9779e
Antonio Alvarado <aalvarado@grupobeh.com>
2022-02-08 16:03:40 -0700

    First commit

---
+++ public/vue/dashboard.js
@@ -0,0 +1,8 @@
+var dashboard = new Vue({
+    el: '#dashboard',
+    data: {
+        message: 'Hello Vue!'
+    },
+    methods: {},
+    created: function () { }
+})
---
+++ resources/views/home.blade.php
@@ -0,0 +1,28 @@
+@extends('layouts.layout')
+
+@section('content')
+<div id="dashboard" class="container">
+    <div class="row justify-content-center">
+        <div class="col-md-8">
+            <div class="card">
+                <div class="card-header">{{ __('Dashboard') }}</div>
+
+                <div class="card-body">
+                    @if (session('status'))
+                    <div class="alert alert-success" role="alert">
+                        {{ session('status') }}
+                    </div>
+                    @endif
+                    @{{ message }} <br>
+                    {{ __('You are logged in!') }}
+                </div>
+            </div>
+        </div>
+    </div>
+</div>
+@endsection
+
+@push('scripts')
+<!-- Vuejs -->
+<script src="vue/dashboard.js"></script>
+@endpush
